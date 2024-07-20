b48fd991ff4cd995e18a88b8ba7e3c6f450b1116
khmersr <khmersr.com>
2024-07-20 11:07:19 +0700

    frist commit

--- App.py
+++ App.py
@@ -1,26 +1,3 @@
-#
-# qty1=int(input("Qty1 ="))
-# qty2= int(input("Qty2 ="))
-# price= float(input("price ="))
-# total_qty=qty1+qty2
-# print=("Total Qty = "+str(total_qty))
-# amount=total_qty*price
-# print("Total Amount= "+str(amount))
-
-
-# a = input()
-# b = input()
-#
-# c = a
-# a = b
-# b = c
-#
-# print("a:" +a)
-# print("b: " +b)
-# print("===========welcome to this==========")
-# battambang = input("Place of Birth =\n")
-# pet = input("Your Pet Name =\n")
-# print("your place")
 
 print("===========String Lesson==========")
 str="hello world"
